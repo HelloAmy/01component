bedd9a92ffe9ea8bece533e6b99f6862ad1c7041
helloAmy <1251759009@qq.com>
2015-12-07 08:45:35 +0800

    日志组件

---
+++ Common.Julius.LoggerComponent/exeTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Julius.LoggerComponent;
+
+namespace exeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrackIDManager.GetInstance("zhujinrong");
+
+            // 控制台程序报错
+            Console.WriteLine("current TrackId：" + TrackIDManager.CurrentTrackID.TrackIdStr);
+
+            TrackIDManager.GetInstance("zhujinrong");
+
+            // 控制台程序报错
+            Console.WriteLine("current TrackId：" + TrackIDManager.CurrentTrackID.TrackIdStr);
+            Console.Read();
+        }
+    }
+}
